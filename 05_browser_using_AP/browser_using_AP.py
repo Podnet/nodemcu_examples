5126c373ef508abff3aa6b0baee5f989b06b2244
Aditya Raman <adityaraman96@gmail.com>
2019-10-07 04:15:44 +0530

    Updated the code with comments

--- 05_browser_using_AP/browser_using_AP.py
+++ 05_browser_using_AP/browser_using_AP.py
@@ -1,8 +1,4 @@
 import usocket as socket
-from machine import UART
-
-uart = UART(0, 115200)                         # init with given baudrate
-uart.init(115200, bits=8, parity=None, stop=1) # init with given parameters
 import network
 import machine
 import time
@@ -13,9 +9,10 @@ esp.osdebug(None)
 import gc
 gc.collect()
 
-ssid = 'ESP8266'
-password = 'nodeesp8266'
+ssid = 'ESP8266' # Name of your network
+password = 'nodeesp8266' # password of your network
 
+# Making NodeMCU Hotspot
 ap = network.WLAN(network.AP_IF)
 ap.active(True)
 ap.config(essid=ssid, password=password)
@@ -26,12 +23,32 @@ while ap.active() == False:
 print('Connection successful')
 print(ap.ifconfig())
 
+# Web Template
 def web_page():
-  html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
-  <body><h1>Hello, World!</h1>
-  <a href=”red"><button>RED</button></a>
-  <a href="green"><button>Green</button></a>
-  <a href="blue"><button>Blue</button></a>
+  html = """<html><head>
+  <style>
+  .button {
+  background-color: black; 
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+}
+  </style>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body><h1 style="text-align:center;">The Podnet</h1>
+  <h2>Press any button to blink LED of yout choice</h2>
+  <a href="colourblink"><button  class="button">Blink All</button></a>
+  <a href=”colourred"><button  class="button button3" style="background-color: #f44336;">Red</button></a>
+  <a href=”colourpink"><button  class="button button6" style="background-color: pink;">Pink</button></a>
+  <a href="colourgreen"><button  class="button" style="background-color: green;">Green</button></a>
+  <a href="colourblue"><button class="button button2" style="background-color: blue;">Blue</button></a>
+  <a href="colourcyan"><button class="button button8" style="background-color: cyan;">Cyan</button></a>
   </body></html>"""
   return html
 
@@ -40,10 +57,12 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
 
+# Initialization of the pins for different colours
 blue = machine.Pin(14, machine.Pin.OUT)
 red = machine.Pin(12, machine.Pin.OUT)
 green = machine.Pin(13, machine.Pin.OUT)
 
+# Starter blinking to authenticate node is working properly
 for i in range(2):
         green.on()
         time.sleep_ms(500)
@@ -54,6 +73,8 @@ for i in range(2):
         red.on()
         time.sleep_ms(500)
         red.off()
+
+# To continue detecting devices
 while True:
   conn, addr = s.accept()
   print('Got a connection from %s' % str(addr))
@@ -61,18 +82,55 @@ while True:
   print('Content = %s' % str(request))
   print("conn = %s" % str(conn))
   str_conn = str(request)
-  if "red" in str_conn:
+  if "colourred" in str_conn:
     red.on()
     blue.off()
     green.off()
-  if "blue" in str_conn:
+  if "colourblue" in str_conn:
     red.off()
     blue.on()
     green.off()
-  if "green" in str_conn:
+  if "colourgreen" in str_conn:
     red.off()
     blue.off()
     green.on()
+  if "colourpink" in str_conn:
+    red.on()
+    blue.on()
+    green.off()
+  if "colouryellow" in str_conn:
+    red.on()
+    blue.off()
+    green.on()
+  if "colourcyan" in str_conn:
+    red.off()
+    blue.on()
+    green.on()
+  if "colourblink" in str_conn:
+    for i in range(5):
+      red.off()
+      blue.off()
+      green.on()
+      time.sleep_ms(300)
+      green.off()
+      red.on()
+      blue.on()
+      time.sleep_ms(300)
+      red.off
+      green.off()
+      blue.on()
+      time.sleep_ms(300)
+      blue.off()
+      green.on()
+      red.on()
+      time.sleep_ms(300)
+      blue.off()
+      green.off()
+      red.on()
+      time.sleep_ms(300)
+      red.off()
+      blue.on()
+      green.on()
   response = web_page()
   conn.send(response)
   conn.close()
